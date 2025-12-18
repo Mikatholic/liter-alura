dc658071f9ecddcc985c38130c185c13171fe8d2
Mikatholic <miguelangelferroescalante@gmail.com>
2025-12-18 14:00:15 -0500

    Corrigiendo

--- src/main/java/com/aluracursos/liter_alura/service/LibroService.java
+++ src/main/java/com/aluracursos/liter_alura/service/LibroService.java
@@ -1,5 +1,6 @@
 package com.aluracursos.liter_alura.service;
 
+import com.aluracursos.liter_alura.model.DatosLibro;
 import com.aluracursos.liter_alura.model.Libro;
 import com.aluracursos.liter_alura.repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ public class LibroService {
     @Autowired
     private LibroRepository repository;
     public void guardarLibro(DatosLibro datosLibro){
-        Optional<Libro> libroExistente = Repository.findByTituloIgnoreCase(datosLibro.titulo());
+        Optional<Libro> libroExistente = repository.findByTituloIgnoreCase(datosLibro.titulo());
 
         if (libroExistente.isPresent()){
             System.out.println("El libro ya está registrado en tu catálogo.");
