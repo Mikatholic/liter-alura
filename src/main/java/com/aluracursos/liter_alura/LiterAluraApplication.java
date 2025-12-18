edc665a63cd181c8d77742e790f52f3055d5f7ca
Mikatholic <miguelangelferroescalante@gmail.com>
2025-12-18 18:45:28 -0500

    Agregando

--- src/main/java/com/aluracursos/liter_alura/LiterAluraApplication.java
+++ src/main/java/com/aluracursos/liter_alura/LiterAluraApplication.java
@@ -1,13 +1,25 @@
 package com.aluracursos.liter_alura;
 
+import com.aluracursos.liter_alura.principal.Principal;
+import com.aluracursos.liter_alura.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiterAluraApplication.class, args);
-	}
+    @Autowired
+    private LibroService libroService;
 
+    public static void main(String[] args) {
+        SpringApplication.run(LiterAluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(libroService);
+        principal.muestraElMenu();
+    }
 }
