f09f4098c4afbd1eb0610a239b3a4fd4f8ebaafa
Mikatholic <miguelangelferroescalante@gmail.com>
2025-12-18 18:45:14 -0500

    Agregando

--- src/main/java/com/aluracursos/liter_alura/service/LibroService.java
+++ src/main/java/com/aluracursos/liter_alura/service/LibroService.java
@@ -1,11 +1,14 @@
 package com.aluracursos.liter_alura.service;
 
+import com.aluracursos.liter_alura.model.Autor;
 import com.aluracursos.liter_alura.model.DatosLibro;
 import com.aluracursos.liter_alura.model.Libro;
+import com.aluracursos.liter_alura.repository.AutorRepository;
 import com.aluracursos.liter_alura.repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,6 +16,10 @@ public class LibroService {
 
     @Autowired
     private LibroRepository repository;
+
+    @Autowired
+    private AutorRepository autorRepository;
+
     public void guardarLibro(DatosLibro datosLibro){
         Optional<Libro> libroExistente = repository.findByTituloIgnoreCase(datosLibro.titulo());
 
@@ -21,7 +28,24 @@ public class LibroService {
         } else{
             Libro libro = new Libro(datosLibro);
             repository.save(libro);
-            System.out.println("Libro guardado con éxito: " + libro.getTitulo());
+            System.out.println(libro);
+            System.out.println("Libro guardado con éxito.");
         }
+    }
+
+    public List<Libro> listarLibrosRegistrados() {
+        return repository.findAll();
+    }
+
+    public List<Autor> listarAutoresRegistrados() {
+        return autorRepository.findAll();
+    }
+
+    public List<Autor> listarAutoresVivosEnAnio(Integer anio) {
+        return autorRepository.autorVivosEnDeterminadoAnio(anio);
+    }
+
+    public List<Libro> listarLibrosPorIdioma(String idioma) {
+        return repository.findByIdioma(idioma);
     }
 }
