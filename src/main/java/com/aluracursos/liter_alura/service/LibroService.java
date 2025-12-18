a1900006a6ce8d063bc8a7042d0e045b4f75e97b
Mikatholic <miguelangelferroescalante@gmail.com>
2025-12-18 13:33:57 -0500

    Creando LibroService.java

---
+++ src/main/java/com/aluracursos/liter_alura/service/LibroService.java
@@ -0,0 +1,26 @@
+package com.aluracursos.liter_alura.service;
+
+import com.aluracursos.liter_alura.model.Libro;
+import com.aluracursos.liter_alura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LibroService {
+
+    @Autowired
+    private LibroRepository repository;
+    public void guardarLibro(DatosLibro datosLibro){
+        Optional<Libro> libroExistente = Repository.findByTituloIgnoreCase(datosLibro.titulo());
+
+        if (libroExistente.isPresent()){
+            System.out.println("El libro ya está registrado en tu catálogo.");
+        } else{
+            Libro libro = new Libro(datosLibro);
+            repository.save(libro);
+            System.out.println("Libro guardado con éxito: " + libro.getTitulo());
+        }
+    }
+}
